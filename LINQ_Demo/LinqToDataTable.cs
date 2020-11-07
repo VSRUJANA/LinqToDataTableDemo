abd672f89e130f353667fa582ceec002095163f2
VSRUJANA <srujana.valavala@gmail.com>
2020-11-08 01:42:40 +0530

    [srujana] Add. Create a DataTable and display it using Linq

--- LINQ_Demo/LinqToDataTable.cs
+++ LINQ_Demo/LinqToDataTable.cs
@@ -16,9 +16,10 @@ namespace LINQ_Demo
             table.Rows.Add("1", "Chai");
             table.Rows.Add("2", "Coffee");
             table.Rows.Add("3", "Milk");
-            DisplayProductsFromTable(table);
+            DisplayDataTable(table);
         }
 
+        // Display Productname column from data table
         public void DisplayProductsFromTable(DataTable table)
         {
             var productNames = from products in table.AsEnumerable() select products.Field<string>("ProductName");
@@ -26,6 +27,17 @@ namespace LINQ_Demo
             foreach (string productName in productNames)
             {
                 Console.WriteLine(productName);
+            }
+        }
+
+        // Display data table
+        public void DisplayDataTable(DataTable table)
+        {
+            var productNames = from products in table.AsEnumerable() select new { ID = products.Field<string>("ID"), Name = products.Field<string>("ProductName") };
+            Console.WriteLine(" ID\tProduct Name");
+            foreach (var productName in productNames)
+            {
+                Console.WriteLine(" "+productName.ID + " \t " + productName.Name);
             }
         }
     }
@@ -42,9 +54,5 @@ namespace LINQ_Demo
 
 
 
-//var productNames = from products in table.AsEnumerable() select new { ID = products.Field<string>("ID"), Name = products.Field<string>("ProductName") };
-//Console.WriteLine("Product Names: ");
-//foreach (var productName in productNames)
-//{
-//    Console.WriteLine(productName.ID + "\t" + productName.Name);
-//}
+
+
