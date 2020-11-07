51863ab283e6d3b48517aaf53998df0654a1cf27
VSRUJANA <srujana.valavala@gmail.com>
2020-11-08 01:27:48 +0530

    Add project files.

---
+++ LINQ_Demo/LinqToDataTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace LINQ_Demo
+{
+    public class LinqToDataTable
+    {
+        public void AddToDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ID");
+            table.Columns.Add("ProductName");
+
+            table.Rows.Add("1", "Chai");
+            table.Rows.Add("2", "Coffee");
+            table.Rows.Add("3", "Milk");
+            DisplayProductsFromTable(table);
+        }
+
+        public void DisplayProductsFromTable(DataTable table)
+        {
+            var productNames = from products in table.AsEnumerable() select products.Field<string>("ProductName");
+            Console.WriteLine("Product Names: ");
+            foreach (string productName in productNames)
+            {
+                Console.WriteLine(productName);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//var productNames = from products in table.AsEnumerable() select new { ID = products.Field<string>("ID"), Name = products.Field<string>("ProductName") };
+//Console.WriteLine("Product Names: ");
+//foreach (var productName in productNames)
+//{
+//    Console.WriteLine(productName.ID + "\t" + productName.Name);
+//}
---
+++ LINQ_Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LINQ_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LINQ Demo!");
+            LinqToDataTable linqToDataTable = new LinqToDataTable();
+            linqToDataTable.AddToDataTable();
+        }
+    }
+}
